2aa794080858b93c154a7c45c91f0c5e8ac692d6
Ana Paula Soler <anapaulasoler711@gmail.com>
2025-08-01 01:27:00 -0300

    deploy

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
-const config = require("../config/config").development;
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
 
 const sequelize = new Sequelize(
   config.database,
